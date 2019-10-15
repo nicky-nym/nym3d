ceb80e2298051dc5afbf4204383e38d07b6820f9
Nicky Nym <54910081+nicky-nym@users.noreply.github.com>
2019-10-15 16:02:41 -0700

    Update nym.py

--- nym.py
+++ nym.py
@@ -12,19 +12,30 @@ from importlib import reload
 
 # from place import Place
 
-import plato as _plato
-from plato import Plato
-
-import merlon as _merlon
-from merlon import Merlon
+import bikeway as _bikeway
+from bikeway import Bikeway
 
 import cottage as _cottage
 from cottage import Cottage
 
-reload(_plato)
-reload(_merlon)
-reload(_cottage)
+import manhattan as _manhattan
+from manhattan import Manhattan
 
+import merlon as _merlon
+from merlon import Merlon
+
+import wurster as _wurster
+from wurster import Wurster
+
+import plato as _plato
+from plato import Plato
+
+reload(_bikeway)
+reload(_cottage)
+reload(_manhattan)
+reload(_merlon)
+reload(_wurster)
+reload(_plato)
 
 if True:
     print("")
@@ -32,12 +43,28 @@ if True:
     plato = Plato()
     plato.delete_all_objects()
 
-    plato.study("Cottage")
+    plato.study("Cottage(s)", x0=-100, y0=100)
     cottage = Cottage(plato)
-    cottage.add_street(1)
+    cottage.add_street(12)
     plato.pontificate()
 
-    plato.study("Merlon")
+    CITY_SIZE = 4
+    plato.study("Manhattan New York", x0=-800*CITY_SIZE, y0=-600*CITY_SIZE)
+    nyc = Manhattan(plato)
+    nyc.add_blocks(CITY_SIZE, CITY_SIZE*2)
+    plato.pontificate()
+
+    plato.study("Merlon Buildings", x0=238, y0=238)
     merlon = Merlon(plato)
-    merlon.add_buildings(2, 2)
+    merlon.add_buildings(8, 8, buildings=True)
+    plato.pontificate()
+
+    plato.study("Bikeways", x0=100, y0=100)
+    bikeway = Bikeway(plato)
+    bikeway.add_bikeways(3, 3, buildings=True)
+    plato.pontificate()
+
+    plato.study("Wurster Hall(s)", x0=100, y0=-600)
+    wurster = Wurster(plato)
+    wurster.add_buildings(1)
     plato.pontificate()
