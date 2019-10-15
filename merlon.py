5052c13eb25bce57ffcf6ad29cabb8faab3c721e
Nicky Nym <54910081+nicky-nym@users.noreply.github.com>
2019-10-15 15:59:57 -0700

    Update merlon.py

--- merlon.py
+++ merlon.py
@@ -17,7 +17,12 @@ from compass_facing import CompassFacing as Facing
 from place import Place
 from plato import Plato
 
-# Distances, in feet
+# Data types
+Landing = Tuple[Num, Iterable[Facing]]  # (landing_altitude, [ramp_facings])
+LANDING_ALTITUDE = 0
+RAMP_FACINGS = 1
+
+# in feet
 STORY_HEIGHT = 10
 ROOFLINE = STORY_HEIGHT * 5
 RAMP_WIDTH = 6
@@ -41,6 +46,10 @@ OCTAGONAL_LANDING = [(-D1, -D2, 0),
                      (+D2, +D1, 0),
                      (-D2, +D1, 0),
                      (-D1, +D2, 0)]
+DIAMOND_CENTER = [(-3, 0, 0),
+                  (0, +3, 0),
+                  (+3, 0, 0),
+                  (0, -3, 0)]
 BASEMENT = [(D1, 0, 0),
             (D1, D2, 0),
             (D2, D1, 0),
@@ -83,6 +92,7 @@ ATTIC = [nudge(APARTMENT[0], dx=-1.2, dy=-2),
          nudge(APARTMENT[6], dx=2, dy=-1.2),
          nudge(APARTMENT[7], dx=1.2, dy=-2)]
 
+
 def _get_cloverleaf_landing_pattern():
     # Make an empty 3-by-3 spatial grid for planning landings and ramps
     landings = [
@@ -91,41 +101,55 @@ def _get_cloverleaf_landing_pattern():
         [[], [], []]
     ]
 
-    # Initialize the landings altitudes and ramps directions
-    landings[1][1].append((00.0, [Facing.NORTH]))
-    landings[1][2].append((02.5, [Facing.EAST]))
-    landings[2][2].append((05.0, [Facing.SOUTH]))
-    landings[2][1].append((07.5, [Facing.WEST]))
+    SOUTH_WEST_FIRST_PATTERN = True
+    if SOUTH_WEST_FIRST_PATTERN:
+        # Start the first spiral on the SOUTH WEST corner
+        landings[1][1].append((00.0, [Facing.WEST]))
+        landings[0][1].append((02.5, [Facing.SOUTH]))
+        landings[0][0].append((05.0, [Facing.EAST, Facing.WEST]))
+        landings[1][0].append((07.5, [Facing.NORTH, Facing.SOUTH]))
 
-    landings[1][1].append((10.0, [Facing.WEST]))
-    landings[0][1].append((12.5, [Facing.NORTH]))
-    landings[0][2].append((15.0, [Facing.EAST]))
-    landings[1][2].append((17.5, [Facing.SOUTH]))
+        landings[1][1].append((10.0, [Facing.NORTH]))
+        landings[1][2].append((12.5, [Facing.WEST]))
+        landings[0][2].append((15.0, [Facing.NORTH, Facing.SOUTH]))
+        landings[0][1].append((17.5, [Facing.EAST, Facing.WEST]))
 
-    landings[1][1].append((20.0, [Facing.SOUTH]))
-    landings[1][0].append((22.5, [Facing.WEST]))
-    landings[0][0].append((25.0, [Facing.NORTH]))
-    landings[0][1].append((27.5, [Facing.EAST]))
+        landings[1][1].append((20.0, [Facing.EAST]))
+        landings[2][1].append((22.5, [Facing.NORTH]))
+        landings[2][2].append((25.0, [Facing.EAST, Facing.WEST]))
+        landings[1][2].append((27.5, [Facing.NORTH, Facing.SOUTH]))
 
-    landings[1][1].append((30.0, [Facing.EAST]))
-    landings[2][1].append((32.5, [Facing.SOUTH]))
-    landings[2][0].append((35.0, [Facing.WEST]))
-    landings[1][0].append((37.5, [Facing.NORTH]))
+        landings[1][1].append((30.0, [Facing.SOUTH]))
+        landings[1][0].append((32.5, [Facing.EAST]))
+        landings[2][0].append((35.0, [Facing.NORTH, Facing.SOUTH]))
+        landings[2][1].append((37.5, [Facing.EAST, Facing.WEST]))
+    else:
+        # Start the first spiral on the NORTH EAST corner
+        landings[1][1].append((00.0, [Facing.NORTH]))
+        landings[1][2].append((02.5, [Facing.EAST]))
+        landings[2][2].append((05.0, [Facing.NORTH, Facing.SOUTH]))
+        landings[2][1].append((07.5, [Facing.EAST, Facing.WEST]))
+
+        landings[1][1].append((10.0, [Facing.WEST]))
+        landings[0][1].append((12.5, [Facing.NORTH]))
+        landings[0][2].append((15.0, [Facing.EAST, Facing.WEST]))
+        landings[1][2].append((17.5, [Facing.NORTH, Facing.SOUTH]))
+
+        landings[1][1].append((20.0, [Facing.SOUTH]))
+        landings[1][0].append((22.5, [Facing.WEST]))
+        landings[0][0].append((25.0, [Facing.NORTH, Facing.SOUTH]))
+        landings[0][1].append((27.5, [Facing.EAST, Facing.WEST]))
+
+        landings[1][1].append((30.0, [Facing.EAST]))
+        landings[2][1].append((32.5, [Facing.SOUTH]))
+        landings[2][0].append((35.0, [Facing.EAST, Facing.WEST]))
+        landings[1][0].append((37.5, [Facing.NORTH, Facing.SOUTH]))
 
     landings[1][1].append((40.0, []))
     return landings
 
 
 def _get_landing_pattern_for_four_cloverleafs():
-    # Make an empty 5-by-5 spatial grid for planning landings and ramps
-    landings = [
-        [[], [], [], [], []],
-        [[], [], [], [], []],
-        [[], [], [], [], []],
-        [[], [], [], [], []],
-        [[], [], [], [], []]
-    ]
-
     def _flip_ramp(ramp, northSouth: bool, eastWest: bool):
         if northSouth and ramp == Facing.NORTH:
             return Facing.SOUTH
@@ -137,23 +161,52 @@ def _get_landing_pattern_for_four_cloverleafs():
             return Facing.EAST
         return ramp
 
-    def _flip_ramps(entries, northSouth: bool, eastWest: bool):
+    def _flip_ramps(landings: Iterable[Landing], northSouth: bool, eastWest: bool):
         out = []
-        for entry in entries:
-            (z, ramps) = entry
+        for landing in landings:
+            (z, ramps) = landing
             flipped_ramps = [_flip_ramp(ramp, northSouth=northSouth, eastWest=eastWest) for ramp in ramps]
             out.append((z, flipped_ramps))
         return out
 
+    # Make an empty 5-by-5 spatial grid for planning landings and ramps
+    landing_pattern = [
+        [[], [], [], [], []],
+        [[], [], [], [], []],
+        [[], [], [], [], []],
+        [[], [], [], [], []],
+        [[], [], [], [], []]
+    ]
     pattern = _get_cloverleaf_landing_pattern()
     for i in range(3):
         for j in range(3):
-            landings[i][j] = pattern[i][j]
-        landings[i][3] = _flip_ramps(pattern[i][1], northSouth=True, eastWest=False)
-        landings[i][4] = _flip_ramps(pattern[i][0], northSouth=True, eastWest=False)
-    landings[3] = [_flip_ramps(entry, northSouth=False, eastWest=True) for entry in landings[1]]
-    landings[4] = [_flip_ramps(entry, northSouth=False, eastWest=True) for entry in landings[0]]
-    return landings
+            landing_pattern[i][j] = pattern[i][j]
+        landing_pattern[i][3] = _flip_ramps(pattern[i][1], northSouth=True, eastWest=False)
+        landing_pattern[i][4] = _flip_ramps(pattern[i][0], northSouth=True, eastWest=False)
+    landing_pattern[3] = [_flip_ramps(landings, northSouth=False, eastWest=True) for landings in landing_pattern[1]]
+    landing_pattern[4] = [_flip_ramps(landings, northSouth=False, eastWest=True) for landings in landing_pattern[0]]
+    return landing_pattern
+
+
+def _trim_edge_ramps_from_pattern(grid: Sequence[Sequence[Iterable[Landing]]]):
+    def _trim_edge_ramps_from_landings(landings: Iterable[Landing], facing: Facing):
+        out = []
+        for landing in landings:
+            (z, ramps) = landing
+            trimmed_ramps = [ramp for ramp in ramps if ramp != facing]
+            out.append((z, trimmed_ramps))
+        return out
+
+    num_rows = len(grid)
+    num_cols = len(grid[0])
+    for column in grid:
+        column[0] = _trim_edge_ramps_from_landings(column[0], Facing.SOUTH)
+        column[-1] = _trim_edge_ramps_from_landings(column[-1], Facing.NORTH)
+    for i, landings in enumerate(grid[0]):
+        grid[0][i] = _trim_edge_ramps_from_landings(landings, Facing.WEST)
+    for i, landings in enumerate(grid[-1]):
+        grid[-1][i] = _trim_edge_ramps_from_landings(landings, Facing.EAST)
+    return grid
 
 
 def _get_landing_pattern(num_rows: int, num_cols: int):
@@ -164,6 +217,7 @@ def _get_landing_pattern(num_rows: int, num_cols: int):
         for j in range(num_cols+1):
             row.append(pattern[i % 4][j % 4])
         grid.append(row)
+    grid = _trim_edge_ramps_from_pattern(grid)
     return grid
 
 
@@ -180,13 +234,15 @@ def _add_roof_around_floor(plato: Plato,
             plato.add_place(Place.ROOF, shape=triangle)
 
 
-def _add_features_at_landing(plato: Plato, ramp_bearings: Sequence, at: Xyz):
+def _add_features_at_landing(plato: Plato, ramp_bearings: Sequence, at: Xyz, buildings: bool=True):
     """Make plato envision the floorspace for a landing and its ramps."""
     (x, y, z) = at
 
     # Landing
     plato.goto(x=x, y=y, z=z, facing=Facing.NORTH)
     plato.add_place(Place.WALKWAY, shape=OCTAGONAL_LANDING)
+    if not buildings and z % 10 == 0:
+        plato.add_place(Place.BARE, shape=DIAMOND_CENTER, wall=3)
 
     # Ramps
     for bearing in ramp_bearings:
@@ -194,7 +250,7 @@ def _add_features_at_landing(plato: Plato, ramp_bearings: Sequence, at: Xyz):
         plato.add_place(Place.WALKWAY, shape=RAMP)
 
     # Floors, Walls, and Roof
-    if z % STORY_HEIGHT == 0:
+    if buildings and z % STORY_HEIGHT == 0:
         for bearing in ramp_bearings:
             # parcel
             plato.goto(x=x, y=y, z=0, facing=bearing)
@@ -222,7 +278,7 @@ class Merlon:
     def __init__(self, plato: Plato):
         self._plato = plato
 
-    def add_buildings(self, num_rows: int=2, num_cols: int=2):
+    def add_buildings(self, num_rows: int=2, num_cols: int=2, buildings: bool=True):
         """Tell plato about all of our landings, ramps, rooms, and roofs."""
         for i, row in enumerate(_get_landing_pattern(num_rows, num_cols)):
             for j, grid_cell in enumerate(row):
@@ -233,5 +289,6 @@ class Merlon:
                     ramp_bearings = landing_spec[1]
                     _add_features_at_landing(self._plato,
                                              ramp_bearings,
-                                             at=(x, y, z))
+                                             at=(x, y, z),
+                                             buildings=buildings)
         return self
